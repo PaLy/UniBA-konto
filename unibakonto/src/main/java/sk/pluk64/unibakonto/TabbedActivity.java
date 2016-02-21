0fc135e20e5701067e6fd212286909e9bcbe0c08
Pavol Lukca <pavol.lukca@gmail.com>
2016-02-21 17:42:22 +0100

    workaround for AccountFragment view stays on screen after destroyView

--- unibakonto/src/main/java/sk/pluk64/unibakonto/TabbedActivity.java
+++ unibakonto/src/main/java/sk/pluk64/unibakonto/TabbedActivity.java
@@ -53,6 +53,7 @@ public class TabbedActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(2);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
