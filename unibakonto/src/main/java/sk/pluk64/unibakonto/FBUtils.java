89daa0179149a83b01795fd297d2f0d53e1bc116
Pavol Lukča <pavol.lukca@gmail.com>
2017-06-06 23:54:14 +0200

    fixed incorrect FB date parser; version 26 (2.1.5)

--- unibakonto/src/main/java/sk/pluk64/unibakonto/FBUtils.java
+++ unibakonto/src/main/java/sk/pluk64/unibakonto/FBUtils.java
@@ -3,9 +3,10 @@ package sk.pluk64.unibakonto;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class FBUtils {
-    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ");
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
 
     public static Date parseDate(String fbTime) {
         try {
